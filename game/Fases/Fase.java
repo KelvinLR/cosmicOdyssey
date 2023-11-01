e612f04f70c0f39075c03665c86846f17b7de292
Rinaldsx <rinaldoigorsm@gmail.com>
2023-11-01 20:57:51 -0300

    feat: Adicionando entidade do segundo jogador

--- game/Fases/Fase.java
+++ game/Fases/Fase.java
@@ -18,9 +18,9 @@ import Player.Player;
 public class Fase extends JPanel implements ActionListener {
     private Image background;
     private Player player1;
+    private Player player2;
     private Timer timer;
-    //private Player player2;
-
+    
     public Fase(String path) {
         setFocusable(true);
         setDoubleBuffered(true);
@@ -28,8 +28,11 @@ public class Fase extends JPanel implements ActionListener {
         ImageIcon ref = new ImageIcon(path);
         background = ref.getImage();
 
-        player1 = new Player();
-        player1.load();
+        player1 = new Player(190, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT);
+        player1.load("assets\\sprites\\players\\p1-spaceship.png");
+
+        player2 = new Player(570, KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_A, KeyEvent.VK_D);
+        player2.load("assets\\sprites\\players\\p2-spaceship.png");
 
         addKeyListener(new TecladoAdapter());
 
@@ -41,12 +44,14 @@ public class Fase extends JPanel implements ActionListener {
         Graphics2D graphics = (Graphics2D) g;
         graphics.drawImage(background, 0, 0, null);
         graphics.drawImage(player1.getImagem(), player1.getX(), player1.getY(), this);
+        graphics.drawImage(player2.getImagem(), player2.getX(), player2.getY(), this);
         g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         player1.update();
+        player2.update();
         repaint();
     }
 
@@ -54,11 +59,13 @@ public class Fase extends JPanel implements ActionListener {
         @Override
         public void keyPressed(KeyEvent tecla) {
             player1.keyPressed(tecla);
+            player2.keyPressed(tecla);
         }
 
         @Override
         public void keyReleased(KeyEvent tecla) {
             player1.keyRelease(tecla);
+            player2.keyRelease(tecla);
         }
     }
 }
