92d51c837225bc765ee77d9559e1ae931624a704
Kelvin Rodrigues <61692363+KelvinLR@users.noreply.github.com>
2023-12-10 14:28:51 -0300

    style: corrigindo identação em Element.java

--- src/elements/Element.java
+++ src/elements/Element.java
@@ -7,18 +7,18 @@ import javax.swing.ImageIcon;
 public abstract class Element {
     protected Image imagem; // Imagem das skins de cada objeto
     protected int x, y; // Posição do objeto na tela
-	protected int largura, altura; // Dimensões do objeto
-	protected boolean isVisible; // Visibilidade do objeto na tela
+    protected int largura, altura; // Dimensões do objeto
+    protected boolean isVisible; // Visibilidade do objeto na tela
 
     // Construtor da classe Element
     public Element(int x, int y, String ref) {
         ImageIcon referencia = new ImageIcon(getClass().getClassLoader().getResource(ref)); // Busca pela referência da imagem
         // Definições dos atributos
-		this.imagem = referencia.getImage();
+	this.imagem = referencia.getImage();
         this.x = x;
-		this.y = y;
-		this.altura = imagem.getHeight(null);
-		this.largura = imagem.getWidth(null);
+	this.y = y;
+	this.altura = imagem.getHeight(null);
+	this.largura = imagem.getWidth(null);
         this.isVisible = true;
     }
 
@@ -27,8 +27,8 @@ public abstract class Element {
 
     // Retorna um retângulo com as dimensões e posição do objeto para interação com outros elementos
     public Rectangle getBounds() {
-		return new Rectangle(x, y, largura, altura);
-	}
+	return new Rectangle(x, y, largura, altura);
+    }
 
     // Métodos getters e setters
     public Image getImagem() {
